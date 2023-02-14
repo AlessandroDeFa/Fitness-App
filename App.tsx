096882ee546e09f961d3c7ad4a207f5fa0fc39df
AlessandroDeFa <aledefaale@gmail.com>
2023-02-14 19:40:44 +0100

    create navbar component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Navbar } from "./components/Navbar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fitness-app</Text>
+      <Text>Fitness-app!</Text>
       <StatusBar style="auto" />
+      <Navbar />
     </View>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#182935",
+    flexDirection: "row",
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+  },
+  containerLink: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textNav: {
+    color: "#FFF",
+    fontSize: 12,
+    fontWeight: "400",
+  },
+  icon: {
+    color: "#FFF",
+    fontSize: 20,
+  },
+});
+
+export const Navbar = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerLink}>
+        <View>
+          <MaterialIcons name="fitness-center" style={styles.icon} />
+        </View>
+        <View>
+          <Text style={styles.textNav}>Esercizi</Text>
+        </View>
+      </View>
+      <View style={styles.containerLink}>
+        <View>
+          <FontAwesome name="plus" style={styles.icon} />
+        </View>
+        <View>
+          <Text style={styles.textNav}>Inizia allenamento</Text>
+        </View>
+      </View>
+      <View style={styles.containerLink}>
+        <View>
+          <FontAwesome5 name="crown" style={styles.icon} />
+        </View>
+        <View>
+          <Text style={styles.textNav}>Aggiorna</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
